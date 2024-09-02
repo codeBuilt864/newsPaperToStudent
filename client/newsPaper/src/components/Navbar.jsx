c1b8c8841e4199b542b14643ce2175126e784949
officialBox <yaseerbostbox@gmail.com>
2024-09-02 22:58:38 +0530

    add react router dom

--- client/newsPaper/src/components/Navbar.jsx
+++ client/newsPaper/src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import "../css/Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="navbar-brand">
+          Book Store
+        </Link>
+      </div>
+      <div className="navbar-right">
+        {/* <Link to='/books' className='navbar-link'>Books</Link> */}
+        <Link to="/contact" className="navbar-link">
+          Contact
+        </Link>
+        {/* <Link to="/service" className="navbar-link">
+          Service
+        </Link> */}
 
-export default Navbar
+        {/* <Link to="/addbook" className="navbar-link">
+          Add Book
+        </Link> */}
+        {/* <Link to="/books" className="navbar-link">
+          Books
+        </Link> */}
+        {/* <Link to="/dashboard" className="navbar-link">
+          Dashboard
+        </Link> */}
+
+        <Link to="/login" className="navbar-link">
+          Login
+        </Link>
+        {/* <Link to="/logout" className="navbar-link">
+          Logout
+        </Link> */}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
