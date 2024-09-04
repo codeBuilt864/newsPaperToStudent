7c8544f703ad91d2103d2ca87e9a52252a21cbf0
officialBox <yaseerbostbox@gmail.com>
2024-09-04 13:40:44 +0530

    complete signup sign in user and admin

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,4 +1,5 @@
 import { Student } from "../models/Student.js";
+import {Admin} from "../models/Admin.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcrypt'
 import jwt from 'jsonwebtoken'
@@ -22,12 +23,24 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try{
-    const validUser = await Student.findOne({ email });
-    if (!validUser) return next(errorHandler(404, 'User not found'))
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validUser = await Student.findOne({ email })
+    const validAdmin = await Admin.findOne({ email })
+    if (validUser){
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, 'Wrong credentials'))
+      const token = jwt.sign({id: validUser._id }, process.env.JWT_SECRET_KEY)
+      const { password: pass, ...rest } = validUser._doc
+      res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest)
+    } 
+    if(!validAdmin) return next(errorHandler(404, "Admin not found"));
+    const validPassword = bcryptjs.compareSync(password, validAdmin.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials'))
-    const token = jwt.sign({id: validUser._id }, process.env.JWT_SECRET_KEY)
-    const { password: pass, ...rest } = validUser._doc
+    const token = jwt.sign({id: validAdmin._id }, process.env.JWT_SECRET_KEY)
+    
+    const { password: pass, ...rest } = validAdmin._doc
     res
     .cookie('access_token', token, { httpOnly: true })
     .status(200)
@@ -36,3 +49,4 @@ export const signin = async (req, res, next) => {
     next(error)
   }
 }
+
